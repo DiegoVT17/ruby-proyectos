202005faf801c2a8b69af904445c994b86d7b0bd
Diego Vera <diegoverat1796@outlook.com>
2025-09-24 09:46:50 -0500

    substring finished

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 str = "Howdy partner, sit down! How's it going?"
 
+
 def find_substring(arr, str)
-  str.downcase.split(" ")
+  cleaned = str.gsub(/[^a-zA-Z0-9\s]/, "").downcase.split(" ")
+  cleaned.reduce(Hash.new(0)) do |result, word|
+    arr.each { |el| result[el] += 1 if word.include?(el) }
+    result
+  end
 end
 
 p find_substring(dictionary, str)
